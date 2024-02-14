12385f062c85730d98468c4f08ee4615e4aede80
Lise Ekhorn Johansen <ljo177@uit.no>
2024-02-14 14:07:11 +0100

    works

--- src/ftclient.py
+++ src/ftclient.py
@@ -41,60 +41,6 @@
 #     download_file(file_name)
 
 ######################################################################################################################33
-# import socket
-# from rsa_encryption import RSAEncryption
-# from aes_encryption import AESEncryption
-
-# HOST = '127.0.0.1'
-# PORT = 12345
-
-# rsa_encryption = RSAEncryption()
-# private_key, public_key = rsa_encryption.generate_rsa_key_pair()
-# aes_encryption = None
-
-# def download_file(file_name):
-#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-#         try:
-#             client_socket.connect((HOST, PORT))
-#             client_socket.settimeout(5)
-
-#             # Send the public RSA key to the server
-#             client_socket.sendall(rsa_encryption.save_public_key(public_key))
-
-#             # Receive the encrypted AES key from the server and decrypt it
-#             encrypted_aes_key = client_socket.recv(1024)
-#             aes_key = rsa_encryption.decrypt(private_key, encrypted_aes_key)
-#             aes_encryption = AESEncryption(aes_key=aes_key)
-
-#             client_socket.sendall(file_name.encode("utf-8"))
-
-#             file_content = b""
-#             while True:
-#                 try:
-#                     data_chunk = client_socket.recv(1024)
-#                     if not data_chunk:
-#                         break
-#                     file_content += data_chunk
-#                 except socket.timeout:
-#                     print("The connection timed out")
-#                     break
-
-#             if file_content.strip() == b"File not found":
-#                 print(f"File {file_name} not found on the server")
-#             else:
-#                 try:
-#                     decrypted_data = aes_encryption.decrypt_aes(file_content)
-#                     print("decrypted_data", decrypted_data)
-#                 except Exception as e:
-#                     print(f"An error occurred during decryption: {e}")
-#                     return
-#         finally:
-#             client_socket.close()
-
-# if __name__ == "__main__":
-#     file_name = "file_name.txt"
-#     download_file(file_name)
-######################################################################################################################33
 
 # client.py
 import socket
@@ -117,6 +63,7 @@ def download_file(file_name):
 
             # Send the public key to the server
             client_socket.sendall(rsa_encryption.serialize_public_key(public_key))
+            print("Public key sent: ", rsa_encryption.serialize_public_key(public_key))
 
             # Receive the encrypted AES key from the server and decrypt it
             encrypted_aes_key = client_socket.recv(1024)
@@ -137,17 +84,19 @@ def download_file(file_name):
             if file_content.strip() == b"File not found":
                 print(f"File {file_name} not found on the server")
             else:
+                print("Encrypted file: ", file_content)
                 # Decrypt the received data
-                decrypted_file_content = aes_encryption.decrypt_aes(file_content)
+                decrypted_file_content = rsa_encryption.decrypt_aes(file_content, private_key)
 
                 # Save the received data to a file
                 with open("client_file.txt", "wb") as file:
                     file.write(decrypted_file_content)
                     print(f"File {file_name} downloaded successfully")
+                    print(f"File content: {decrypted_file_content}")
 
         except Exception as e:
             print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    file_name = "file_name.txt"
+    file_name = "message.txt"
     download_file(file_name)
--- src/rsa_encryption.py
+++ src/rsa_encryption.py
@@ -109,10 +109,14 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+import os
 
 class RSAEncryption:
-    def __init__(self):
+    def __init__(self, aes_key=None):
         self.private_key, self.public_key = self.generate_rsa_key_pair()
+        self.aes_key = aes_key
 
     def generate_rsa_key_pair(self):
         private_key = rsa.generate_private_key(
@@ -155,3 +159,49 @@ class RSAEncryption:
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         )
         return pem
+
+    def encrypt_aes(self, plaintext, public_key):
+        generate_symmetric_key = os.urandom(16)
+        print("Symmetric key: ", generate_symmetric_key)
+        print("Public key: ", public_key)
+
+        # Encrypt the symmetric key with the public key
+        encrypted_symmetric_key = public_key.encrypt(
+            generate_symmetric_key,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+        print("Encrypted symmetric key: ", encrypted_symmetric_key)
+
+        iv = os.urandom(16)
+        
+        cipher = Cipher(algorithms.AES(generate_symmetric_key), modes.CFB(iv), backend=default_backend())
+        encryptor = cipher.encryptor()
+        ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+
+        return iv + encrypted_symmetric_key + ciphertext
+
+    def decrypt_aes(self, ciphertext, private_key):
+        # Split up the ciphertext into the iv, encrypted symmetric key and the actual ciphertext
+        iv = ciphertext[:16]
+        encrypted_symmetric_key = ciphertext[16:272]
+        ciphertext = ciphertext[272:]
+
+        # Decrypt the symmetric key with the private key
+        symmetric_key = private_key.decrypt(
+            encrypted_symmetric_key,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+
+        cipher = Cipher(algorithms.AES(symmetric_key), modes.CFB(iv), backend=default_backend())
+        decryptor = cipher.decryptor()
+        plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+        return plaintext
